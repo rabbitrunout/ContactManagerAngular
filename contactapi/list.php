46726680dcd9d0ee7ce9835ae8b4936b5d75e78e
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2025-06-25 09:30:12 -0400

    Added list.php

---
+++ contactapi/list.php
@@ -0,0 +1,31 @@
+<?php
+    require 'connect.php';
+
+    $contacts = [];
+    $sql = "SELECT contactID, firstName, emailAddress, phone, status, dob, imageName, typeID FROM contacts";
+
+    if ($result = mysqli_query($con, $sql))
+    {
+        $count = 0;
+        while ($row = mysqli_fetch_assoc($result))
+        {
+            $contacts[$count]['contactID'] = $row['contactID'];
+            $contacts[$count]['firstName'] = $row['firstName'];
+            $contacts[$count]['lastName'] = $row['lastName'];
+            $contacts[$count]['emailAddress'] = $row['emailAddress'];
+            $contacts[$count]['phone'] = $row['phone'];
+            $contacts[$count]['status'] = $row['status'];
+            $contacts[$count]['dob'] = $row['dob'];
+            $contacts[$count]['imageName'] = $row['imageName'];
+            $contacts[$count]['typeID'] = $row['typeID'];
+
+            $count++;
+        }
+
+        echo json_encode(['data'=>$contacts]);
+    }
+    else
+    {
+        http_response_code(404);
+    }
+?>
